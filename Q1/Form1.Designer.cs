5f669cd72becc1eaa475eeff84eab9af7f29c278
ispan <ispan@DESKTOP-PAKSETB>
2022-10-26 12:57:51 +0800

    加入專案檔案。

---
+++ Q1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Q1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Nowbutton = new System.Windows.Forms.Button();
+			this.Secondbutton = new System.Windows.Forms.Button();
+			this.Timelabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// Nowbutton
+			// 
+			this.Nowbutton.Location = new System.Drawing.Point(115, 205);
+			this.Nowbutton.Name = "Nowbutton";
+			this.Nowbutton.Size = new System.Drawing.Size(75, 23);
+			this.Nowbutton.TabIndex = 0;
+			this.Nowbutton.Text = "First";
+			this.Nowbutton.UseVisualStyleBackColor = true;
+			this.Nowbutton.Click += new System.EventHandler(this.Nowbutton_Click);
+			// 
+			// Secondbutton
+			// 
+			this.Secondbutton.Location = new System.Drawing.Point(284, 205);
+			this.Secondbutton.Name = "Secondbutton";
+			this.Secondbutton.Size = new System.Drawing.Size(75, 23);
+			this.Secondbutton.TabIndex = 0;
+			this.Secondbutton.Text = "Second";
+			this.Secondbutton.UseVisualStyleBackColor = true;
+			this.Secondbutton.Click += new System.EventHandler(this.Secondbutton_Click);
+			// 
+			// Timelabel
+			// 
+			this.Timelabel.AutoSize = true;
+			this.Timelabel.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.Timelabel.Location = new System.Drawing.Point(25, 107);
+			this.Timelabel.Name = "Timelabel";
+			this.Timelabel.Size = new System.Drawing.Size(87, 29);
+			this.Timelabel.TabIndex = 1;
+			this.Timelabel.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(473, 285);
+			this.Controls.Add(this.Timelabel);
+			this.Controls.Add(this.Secondbutton);
+			this.Controls.Add(this.Nowbutton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Nowbutton;
+		private System.Windows.Forms.Button Secondbutton;
+		private System.Windows.Forms.Label Timelabel;
+	}
+}
+
---
+++ Q1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			Timelabel.Text = string.Empty;
+		}
+
+		private void Nowbutton_Click(object sender, EventArgs e)
+		{
+			DateTime now = DateTime.Now;
+			GetTime(now);
+			string time = GetTime(now) + now.ToString("yyyy/MM/dd HH:mm:ss");
+			Timelabel.Text = time;
+		}
+
+		private void Secondbutton_Click(object sender, EventArgs e)
+		{
+			DateTime now = DateTime.Now;
+			GetTime(now);
+			string time = GetTime(now) +"\r\n" +now.ToString("yyyy/MM/dd HH:mm:ss");
+			Timelabel.Text = time;
+		}
+		private string GetTime(DateTime now)
+		{
+			string hour = now.ToString("HH");
+			int.TryParse(hour, out int hey);
+			if (hey < 12)
+			{
+				string word = "早安";
+				return word;
+			}
+			if (hey < 18)
+			{
+				string word = "午安";
+				return word;
+			}
+			else
+			{
+				string word = "晚安";
+				return word;
+			}
+
+		}
+	}
+}
